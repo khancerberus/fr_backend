6614eb912f6db5daafae7df4d04e71daa111e7a8
Victor Valenzuela <vvalenzuela@itsanet.cl>
2023-03-04 00:53:31 -0300

    Added db and migrate to project

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 def generate_app(settings='config.development'):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(settings)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     register_blueprints(app)
 
